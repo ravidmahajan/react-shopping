3490f38e306ba77ee944ab16caf71fc64d8d686b
Ravi Mahajan <ravi.d.mahajan@gmail.com>
2024-03-26 18:55:44 +0530

    React components + building react component +JSX rules

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
 import App from './App';
+import { NetflixRegisterComponent } from './components/NetflixRegisterComponent';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <NetflixRegisterComponent />
   </React.StrictMode>
 );
 
